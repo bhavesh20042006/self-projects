5c5ece77b0aca0d222101c6db47d4a77a57c42e3
bhavesh20042006 <bhaveshkhullar9810@gmail.com>
2024-09-14 10:36:15 +0530

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("welcome to the band name generator.")
+city = input("whats the name of the city you grew up in ?")
+print(city)
+pet = input("what's the name of your pet?")
+print(pet)
+print("your band name could be :" + city + " " + pet)
